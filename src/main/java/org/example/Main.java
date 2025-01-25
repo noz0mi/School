82063bf2dc0e55cc3d71cdca5b55c2e4d8853afa
noz0mi <reez.evas@gmail.com>
2025-01-25 05:04:09 +0300

    small fixes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,15 +7,11 @@ import java.util.Scanner;
 
 public class Main {
 
-public static boolean correctFile = false;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите команды и файлы для фильтрации");
         String line = scanner.nextLine();
-
-        // String line = "-s -o /some/newpath -a -p result_ in1.txt in2.txt in3.txt";
-
         ArrayList<Data> collected = UtilityParser.parsingFiles(line);
             while (collected.isEmpty()) {
                 System.out.println("Введите команды и файлы для фильтрации по примеру: " +
